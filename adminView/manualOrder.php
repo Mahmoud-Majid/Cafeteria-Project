3ca9bad499f5a090391ccd86796d6cd816103b21
AbdlefattahOmar <abdlefattahomar66@gmail.com>
2022-04-22 02:01:50 +0200

    orders

--- adminView/manualOrder.php
+++ adminView/manualOrder.php
@@ -1,24 +1,24 @@
-<?php 
+<?php
 
-session_start();
-// If the user is not logged in redirect to the login page...
-if (!isset($_SESSION['loggedin'])) {
-    header('Location: ../login.php');
-}
-if ($_SESSION['is_admin']!=1){
-    die ("Access Denied");
-}
+// session_start();
+// // If the user is not logged in redirect to the login page...
+// if (!isset($_SESSION['loggedin'])) {
+//     header('Location: ../login.php');
+// }
+// if ($_SESSION['is_admin']!=1){
+//     die ("Access Denied");
+// }
 
 ?>
 
 <?php
 
-    include '../pdo.php';
-    
-    //select user
-    $query = "SELECT user_id,username FROM `user`";
-    $stmt = $db->query($query);
-    
+include '../pdo.php';
+
+//select user
+$query = "SELECT user_id,username FROM `user`";
+$stmt = $db->query($query);
+
 
 ?>
 <!DOCTYPE html>
@@ -28,8 +28,7 @@ if ($_SESSION['is_admin']!=1){
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/manualOrder.css" />
     <link rel="stylesheet" href="../css/adminNav.css" />
 
@@ -40,22 +39,21 @@ if ($_SESSION['is_admin']!=1){
 
 <body>
 
-    <?php include('adminNav.html') ?>
+    <?php include('../navbars/admin_header.php') ?>
 
     <div class="main">
-        <form class="order-data" id="form" action="adminInsertOrder.php" method="post">
+        <form class="order-data" id="form" action="addOrderToUser.php" method="post">
             <div class="order">
                 <div id="list"></div>
             </div>
             <?php
             echo "<select name='user' id='user'>";
-                while ($ele = $stmt->fetch()) 
-                {
-                    $user_id = $ele['user_id'];
-                    $username = $ele['username'];
-                    echo "<option value='$user_id'>$username</option>";
-                }
-            echo"</select>";
+            while ($ele = $stmt->fetch()) {
+                $user_id = $ele['user_id'];
+                $username = $ele['username'];
+                echo "<option value='$user_id'>$username</option>";
+            }
+            echo "</select>";
             ?>
             <div class="notes">
                 <label id="notes" for="notes">Notes:</label>
@@ -80,8 +78,7 @@ if ($_SESSION['is_admin']!=1){
         </form>
         <div class="product-list-addUser">
             <div class="d-flex">
-                <input class="form-control me-2 search-bar" type="search" placeholder="Search" aria-label="Search"
-                    name='search' id='search'>
+                <input class="form-control me-2 search-bar" type="search" placeholder="Search" aria-label="Search" name='search' id='search'>
             </div>
             <?php
             $query = "SELECT product_id,name,price,pic FROM product";
@@ -89,13 +86,12 @@ if ($_SESSION['is_admin']!=1){
 
             echo "<div class='items-list '>";
             while ($ele = $stmt->fetch()) {
-                echo (
-                "<div class='item'>
+                echo ("<div class='item'>
                     <img class='item-img' data-price={$ele['price']} data-name={$ele['name']} data-id={$ele['product_id']} src='../images/{$ele['pic']}' />
                     <div>{$ele['name']}</div>
                     <div>{$ele['price']}$</div>
                 </div>"
-            );
+                );
             }
             echo "</div>";
             echo "</div>";
