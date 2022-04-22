f9e13d895074aa00679c247a9efcde73cc0433c4
AbdlefattahOmar <abdlefattahomar66@gmail.com>
2022-04-22 22:56:30 +0200

    all users

--- navbars/admin_header.php
+++ navbars/admin_header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -12,25 +13,27 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
   <title>Java</title>
 </head>
+
 <body>
-<div class="container-fluid px-0">
+  <div class="container-fluid px-0">
     <!-- <nav class="navbar navbar-expand-sm navbar-warning navbar-fixed-top bg-black py-0 px-0"> <a class="navbar-brand" href="#"><img id="logo" src="../images/logo.png"> &nbsp;&nbsp;&nbsp;</a> <span class="v-line"></span> <button class="navbar-toggler mr-3" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button> -->
     <nav class="navbar navbar-expand-sm navbar-warning navbar-fixed-top bg-black py-0 px-0"> <a class="navbar-brand nav-link" id="logo" href="#">Java Cafe &nbsp;&nbsp;&nbsp;</a> <span class="v-line"></span> <button class="navbar-toggler mr-3" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item active"> <a class="nav-link" href="../adminView/orders.php">Home</a> </li>
-                <li class="nav-item"> <a class="nav-link" href="../adminView/list_products.php">Products</a> </li>
-                <li class="nav-item"> <a class="nav-link" href="#">Users</a> </li>
-                <li class="nav-item"> <a class="nav-link" href="../adminView/manualOrder.php">Manual Orders</a> </li>
-                <li class="nav-item"> <a class="nav-link" href="../adminView/checks.php">Checks</a> </li>
-                <li class="nav-item ml-auto"> <a class="nav-link" href="#">Admin</a> </li>
-                <li class="nav-item"><img src="../images/admin.png" alt="admin" class="avatar"/></li>
-                <li class="nav-item "><a class="nav-link" href="../logout.php">Logout</a></li>
-              </ul>
-        </div>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active"> <a class="nav-link" href="../adminView/orders.php">Home</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="../adminView/list_products.php">Products</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="../adminView/list_users.php">Users</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="../adminView/manualOrder.php">Manual Orders</a> </li>
+          <li class="nav-item"> <a class="nav-link" href="../adminView/checks.php">Checks</a> </li>
+          <li class="nav-item ml-auto"> <a class="nav-link" href="#">Admin</a> </li>
+          <li class="nav-item"><img src="../images/admin.png" alt="admin" class="avatar" /></li>
+          <li class="nav-item "><a class="nav-link" href="../logout.php">Logout</a></li>
+        </ul>
+      </div>
     </nav>
-</div>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script> 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> 
+  </div>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </body>
+
 </html>
